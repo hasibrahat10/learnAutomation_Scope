1aa096edc429b2f9e8225e6915660fa0e00363b7
hasibrahat10 <hasibrahat10@gmail.com>
2021-01-07 18:07:23 +0600

    Homesteps

--- src/test/java/stepDefinitions/HomeSteps.java
+++ src/test/java/stepDefinitions/HomeSteps.java
@@ -9,19 +9,6 @@ import pageObject.HomePage;
 public class HomeSteps extends BaseClass {
     WebDriver driver;
 
-
-    public void user_Launch_Chrome_browser() {
-        System.setProperty("webdriver.chrome.driver","C://Drivers//chromedriver_win32/chromedriver.exe");
-        driver = new ChromeDriver(); //launch chrome browser
-        homePage =new HomePage(driver);
-
-    }
-
-    @When("User open URL {string}")
-    public void user_open_URL(String url) {
-        driver.get(url);
-        driver.manage().window().maximize();
-    }
     @When("User click on Women Category")
     public void user_click_on_Women_Category() {
         homePage.clickOnWomenBtn();
